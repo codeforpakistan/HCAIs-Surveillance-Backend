6528aced996e2946f9709bca115d877d9c85c528
sohail0992 <msohail.se@gmail.com>
2022-02-07 13:52:05 +0500

    deploy .sh file

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ echo "Deploying the application ..."
 source ~/.nvm/nvm.sh
 npm install -g yarn
 yarn instal
-pm2 startOrRestart ecosystem.config.js --name hcai-backend
+pm2 startOrRestart yarn run watch
+
 echo "Deployment complete."
