a8c4e65bd1dd439dc2961c44f25f4b5061dec5cf
Muhammad Sohail <msohail.se@gmail.com>
2022-02-07 14:09:58 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo "Deploying the application ..."
 source ~/.nvm/nvm.sh
 npm install -g yarn
 yarn instal
-pm2 start yarn --name api -- watch
+echo "Running the application"
+pm2 startOrRestart yarn --name api -- watch
 
 echo "Deployment complete."
