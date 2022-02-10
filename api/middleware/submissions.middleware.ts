9f9e8ddad4187c17ee9a9e020dde1c4467260ccb
Mubassir Hayat <mubassirhayat@gmail.com>
2022-02-10 15:08:33 +0500

    submission middleware added

---
+++ api/middleware/submissions.middleware.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import submissionService from '../services/submission.service';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:hospitals-controller');
+class SubmissionsMiddleware {
+
+    async validateRequiredSubmissionBodyFields(req: express.Request, res: express.Response, next: express.NextFunction) {
+        if (req.body && req.body.hospitalId && req.body.userId && req.body.departmentId, req.body.unitId) {
+            next();
+        } else {
+            res.status(400).send({error: `Missing required fields hospitalId, userId, departmentId, unitId`});
+        }
+    }
+
+    async validateSubmissionExists(req: express.Request, res: express.Response, next: express.NextFunction) {
+        const submission = await submissionService.readById(req.params.submissionId);
+        if (submission) {
+            next();
+        } else {
+            res.status(404).send({error: `HCAI Submission ${req.params.submissionId} not found`});
+        }
+    }
+
+    async extractSubmissionId(req: express.Request, res: express.Response, next: express.NextFunction) {
+        req.body.id = req.params.submissionId;
+        next();
+    }
+}
+
+export default new SubmissionsMiddleware();
