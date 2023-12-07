20acea4629451fd32e15dc8e11803908f60f3104
sohail0992 <msohail.se@gmail.com>
2023-12-07 19:24:38 +0500

    hcai fix

--- app.ts
+++ app.ts
@@ -1,26 +1,24 @@
-import 'dotenv/config'
-import express, { RequestHandler } from 'express';
-import bodyParser from 'body-parser';
-import session from 'express-session';
 import MongoStore from 'connect-mongo';
-import * as http from 'http';
-import * as winston from 'winston';
-import * as expressWinston from 'express-winston';
-import cors from 'cors'
-import {CommonRoutesConfig} from './common/common.routes.config';
-import { UsersRoutes } from './api/routers/users.routes.config';
-import { HospitalsRoutes } from './api/routers/hospitals.routes.config';
-import { HcaiRoutes } from './api/routers/hcai.routes.config';
+import cors from 'cors';
 import debug from 'debug';
+import 'dotenv/config';
+import express, { RequestHandler } from 'express';
+import session from 'express-session';
+import * as expressWinston from 'express-winston';
+import * as http from 'http';
 import mongoose from 'mongoose';
-import { SubmissionRoutes } from './api/routers/submission.routs.config';
-import { ICDRoutes } from './api/routers/icd-codes.routes.config';
 import passport from 'passport';
+import * as winston from 'winston';
+import { HcaiRoutes } from './api/routers/hcai.routes.config';
+import { HospitalsRoutes } from './api/routers/hospitals.routes.config';
+import { ICDRoutes } from './api/routers/icd-codes.routes.config';
+import { SubmissionRoutes } from './api/routers/submission.routs.config';
+import { UsersRoutes } from './api/routers/users.routes.config';
+import { CommonRoutesConfig } from './common/common.routes.config';
 // API keys and Passport configuration
-import { isAuthenticated } from './api/config/passportConfig';
-import { OrganismRoutes } from './api/routers/organisms.routes.config';
 import { AntibioticRoutes } from './api/routers/antibiotics.routes.config';
 import { DraftRoutes } from './api/routers/drafts.routes.config';
+import { OrganismRoutes } from './api/routers/organisms.routes.config';
 const mongoUrl = process.env.DB_URL || 'localhost:27017/hcai';
 mongoose.connect(mongoUrl);
 const db = mongoose.connection;
